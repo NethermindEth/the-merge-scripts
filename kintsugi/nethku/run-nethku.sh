a720727eeafe5820bac1ceb6e0e7f5ee77e2a11e
Tomasz Kajetan Stańczak <tkstanczak@users.noreply.github.com>
2022-01-06 21:14:55 +0000

    Update run-nethku.sh
    update eth2 to eth

--- kintsugi/nethku/run-nethku.sh
+++ kintsugi/nethku/run-nethku.sh
@@ -78,7 +78,7 @@ services:
         - ./beacon_data:/consensus_data
         - ~/merge-testnets/kintsugi:/custom_config_data
     command: >
-        --data-path "datadir-teku"   --network https://github.com/eth2-clients/merge-testnets/raw/main/kintsugi/config.yaml   --initial-state https://github.com/eth2-clients/merge-testnets/raw/main/kintsugi/genesis.ssz   --Xee-endpoint http://localhost:8550   --p2p-discovery-bootnodes "enr:-Iq4QKuNB_wHmWon7hv5HntHiSsyE1a6cUTK1aT7xDSU_hNTLW3R4mowUboCsqYoh1kN9v3ZoSu_WuvW9Aw0tQ0Dxv6GAXxQ7Nv5gmlkgnY0gmlwhLKAlv6Jc2VjcDI1NmsxoQK6S-Cii_KmfFdUJL2TANL3ksaKUnNXvTCv1tLwXs0QgIN1ZHCCIyk"
+        --data-path "datadir-teku"   --network https://github.com/eth-clients/merge-testnets/raw/main/kintsugi/config.yaml   --initial-state https://github.com/eth-clients/merge-testnets/raw/main/kintsugi/genesis.ssz   --Xee-endpoint http://localhost:8550   --p2p-discovery-bootnodes "enr:-Iq4QKuNB_wHmWon7hv5HntHiSsyE1a6cUTK1aT7xDSU_hNTLW3R4mowUboCsqYoh1kN9v3ZoSu_WuvW9Aw0tQ0Dxv6GAXxQ7Nv5gmlkgnY0gmlwhLKAlv6Jc2VjcDI1NmsxoQK6S-Cii_KmfFdUJL2TANL3ksaKUnNXvTCv1tLwXs0QgIN1ZHCCIyk"
     network_mode: host
 ' > ~/docker-compose.nethbus.yml
 
