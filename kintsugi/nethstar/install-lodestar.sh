a204d7541d4b7376d444deab3f49402c001b9489
Miguel Tenorio <mtpotro41@gmail.com>
2022-01-27 13:49:36 -0500

    feat: Add dependencies install instructions for lodestar

--- kintsugi/nethstar/install-lodestar.sh
+++ kintsugi/nethstar/install-lodestar.sh
@@ -3,6 +3,16 @@
 #For bash errors
 set -eo pipefail
 
+# npm
+apt install -y npm
+
+# upgrade node
+npm install -g n
+n 15.0.1
+
+# yarn 
+npm install --global yarn
+
 git clone https://github.com/chainsafe/lodestar.git
 cd lodestar
 yarn install --ignore-optional
